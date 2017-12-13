be4d967e5672b17d565bb35be85867feff67d7d7
DESKTOP-9OG7B8R\kwan <dev.kks0211@gmail.com>
2017-12-13 21:39:15 +0900

    Test

--- src/main/java/org/hanseungroom/copymedium/CopyMediumApplication.java
+++ src/main/java/org/hanseungroom/copymedium/CopyMediumApplication.java
@@ -2,11 +2,24 @@ package org.hanseungroom.copymedium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CopyMediumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CopyMediumApplication.class, args);
+	}
+
+}
+
+@Controller
+@RequestMapping("/")
+class Test1 {
+
+	@RequestMapping("")
+	public String Test() {
+		return "index";
 	}
 }
