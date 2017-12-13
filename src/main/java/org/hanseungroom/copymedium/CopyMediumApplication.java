7eabc8d452e842c24f5816246b6537cd77e513c3
artiveloper <artiveloper@gmail.com>
2017-12-13 20:52:54 +0900

    first commit
    Signed-off-by: artiveloper <artiveloper@gmail.com>

---
+++ src/main/java/org/hanseungroom/copymedium/CopyMediumApplication.java
@@ -0,0 +1,12 @@
+package org.hanseungroom.copymedium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyMediumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyMediumApplication.class, args);
+	}
+}
